aa0ccfb92e2e9ad034cd4fdac9f62f12394c770c
Jisoo Lee <jisoo.h.lee@gmail.com>
2025-06-30 20:35:33 +0900

    basic leader keys and window operation

--- init.lua
+++ init.lua
@@ -1,94 +1,64 @@
---[[
+vim.g.mapleader = ' ' -- 메인 리더키: Space
+vim.g.maplocalleader = '\\' -- 로컬 리더키: Backslash (기본값)
 
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
+-- 커스텀 리더키들 설정
+local function setup_custom_leaders()
+  -- Comma(,) 키 - 즐겨찾기 파일 접근용
+  vim.keymap.set('n', ',e', function()
+    vim.cmd('edit ' .. vim.fn.stdpath 'config' .. '/init.lua')
+  end, { desc = 'Edit init.lua' })
 
-What is Kickstart?
+  -- 추가 즐겨찾기 파일들 (필요에 따라 추가)
+  -- vim.keymap.set('n', ',v', ':edit ~/.vimrc<CR>', { desc = 'Edit vimrc' })
+  -- vim.keymap.set('n', ',z', ':edit ~/.zshrc<CR>', { desc = 'Edit zshrc' })
 
-  Kickstart.nvim is *not* a distribution.
+  -- t 키 - 윈도우 관리용 prefix
+  -- t' = vertical split with same buffer
+  vim.keymap.set('n', "t'", function()
+    vim.cmd 'vsplit'
+  end, { desc = 'Vertical split (same buffer)' })
 
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
+  -- t- = horizontal split with same buffer
+  vim.keymap.set('n', 't-', function()
+    vim.cmd 'split'
+  end, { desc = 'Horizontal split (same buffer)' })
 
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
+  -- 추가 윈도우 관리 기능들
+  vim.keymap.set('n', 'tq', '<C-w>q', { desc = 'Quit window' })
+  vim.keymap.set('n', 'to', '<C-w>o', { desc = 'Only this window' })
+  vim.keymap.set('n', 'th', '<C-w>h', { desc = 'Move to left window' })
+  vim.keymap.set('n', 'tj', '<C-w>j', { desc = 'Move to bottom window' })
+  vim.keymap.set('n', 'tk', '<C-w>k', { desc = 'Move to top window' })
+  vim.keymap.set('n', 'tl', '<C-w>l', { desc = 'Move to right window' })
 
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
+  -- 기존 Space 기반 리더키들
+  -- 버퍼 관리용 prefix: <leader>b
+  vim.keymap.set('n', '<leader>b', '<nop>', { desc = 'Buffer operations' })
 
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+  -- Git 관련 prefix: <leader>g
+  vim.keymap.set('n', '<leader>g', '<nop>', { desc = 'Git operations' })
 
-Kickstart Guide:
+  -- 디버깅 prefix: <leader>d
+  vim.keymap.set('n', '<leader>d', '<nop>', { desc = 'Debug operations' })
 
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+  -- Emacs-style 키 바인딩
+  -- colon과 semicolon 바꾸기 (colon을 더 자주 사용)
+  vim.keymap.set('n', ';', ':', { desc = 'Command mode (easier access)' })
+  vim.keymap.set('n', ':', ';', { desc = 'Repeat last f, F, t, T' })
 
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
+  -- a와 A 바꾸기 (라인 끝 이동을 더 쉽게)
+  vim.keymap.set('n', 'a', 'A', { desc = 'Insert at end of line' })
+  vim.keymap.set('n', 'A', 'a', { desc = 'Insert after cursor' })
+end
 
-    (If you already know the Neovim basics, you can skip this step.)
+-- 커스텀 리더키들 설정
+setup_custom_leaders()
 
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- 버퍼 관리 (Space 기반)
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = '[N]ext buffer' })
+vim.keymap.set('n', '<leader>bp', ':bprev<CR>', { desc = '[P]revious buffer' })
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = '[D]elete buffer' })
+vim.keymap.set('n', '<leader>bl', ':buffers<CR>', { desc = '[L]ist buffers' })
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -99,7 +69,7 @@ vim.g.have_nerd_font = false
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.o.number = true
+vim.o.number = false
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
@@ -347,6 +317,14 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>b', group = '[B]uffer operations' },
+        { '<leader>g', group = '[G]it operations' },
+        { '<leader>d', group = '[D]ebug operations' },
+        { '<leader>?', group = 'Key mappings' },
+        -- Comma 키 그룹 (즐겨찾기 파일)
+        { ',', group = 'Quick files' },
+        -- t 키 그룹 (윈도우 관리)
+        { 't', group = 'Window operations' },
       },
     },
   },
